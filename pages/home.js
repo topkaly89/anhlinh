c6bed46868f41a3d8fc4642c0f3a96c6fd908a1a
topkaly89 <allmylove00@gmail.com>
2021-09-15 00:26:01 +0700

    Add files via upload

--- pages/home.js
+++ pages/home.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-import ReactFullpage from '@fullpage/react-fullpage'
+//import ReactFullpage from '@fullpage/react-fullpage'
 import Section1 from '../components/Section1'
 import Section2 from '../components/Section2'
 import Section3 from '../components/Section3'
@@ -17,23 +17,11 @@ export default function Home() {
                 <script src="/js/main.js"></script>
 			</Head>
 
-			<ReactFullpage
-                licenseKey = {'YOUR_KEY_HERE'}
-                scrollingSpeed = {1000}
-                render = {
-                    ({ state, fullpageApi }) => {
-                        return (
-                            <ReactFullpage.Wrapper>
-                                <Section1 fullpageApi={fullpageApi} />
-                                <Section3 fullpageApi={fullpageApi} />
-                                <Section4 fullpageApi={fullpageApi} />
-                                <Section5 fullpageApi={fullpageApi} />
-                                <Section2 fullpageApi={fullpageApi} />
-                            </ReactFullpage.Wrapper>
-                        )
-                    }
-                }
-            />
+			<Section1 fullpageApi={fullpageApi} />
+            <Section3 fullpageApi={fullpageApi} />
+            <Section4 fullpageApi={fullpageApi} />
+            <Section5 fullpageApi={fullpageApi} />
+            <Section2 fullpageApi={fullpageApi} />
 		</div>
 	)
 }

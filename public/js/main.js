3c92ab20c0cb0f3cb6483f981258c5d5d1d0bdfe
topkaly89 <allmylove00@gmail.com>
2021-09-15 00:53:28 +0700

    Add files via upload

--- public/js/main.js
+++ public/js/main.js
@@ -3,4 +3,18 @@ $(function(){
     $(document).on('click', '#menu', function() {
         $('#menulist').toggleClass("hide--mobile");
     });
+
+
+
+    $('#fullpage').fullpage({
+		//options here
+		autoScrolling:true,
+		scrollHorizontally: true
+	});
+
+	//methods
+	$.fn.fullpage.setAllowScrolling(false);
+
+
+    
 });

6b374cd3901ba178be0747dd96ecc298d07fa939
topkaly89 <allmylove00@gmail.com>
2021-09-14 23:06:10 +0700

    Add files via upload

--- pages/home.js
+++ pages/home.js
@@ -6,10 +6,6 @@ import Section2 from '../components/Section2'
 import Section3 from '../components/Section3'
 import Section4 from '../components/Section4'
 import Section5 from '../components/Section5'
-
-import 'js/jquery.min.js';
-import 'js/main.js';
-
 export default function Home() {
 	return (
 		<div>
@@ -17,8 +13,8 @@ export default function Home() {
 				<title>AstroV - Take Off and Go Beyond</title>
 				<meta name="description" content="AstroV" />
 				<link rel="icon" href="/favicon.ico" />
-                <script src="js/jquery.min.js"></script>
-                <script src="js/main.js"></script>
+                <script src="/js/jquery.min.js"></script>
+                <script src="/js/main.js"></script>
 			</Head>
 
 			<ReactFullpage

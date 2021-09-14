6a32a8a6b3e989c1eb9d4fbaa2b5c1bedfe0c518
topkaly89 <allmylove00@gmail.com>
2021-09-15 00:47:02 +0700

    Add files via upload

--- pages/home.js
+++ pages/home.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-//import ReactFullpage from '@fullpage/react-fullpage'
+import ReactFullpage from '@fullpage/react-fullpage'
 import Section1 from '../components/Section1'
 import Section2 from '../components/Section2'
 import Section3 from '../components/Section3'
@@ -13,15 +13,22 @@ export default function Home() {
 				<title>AstroV - Take Off and Go Beyond</title>
 				<meta name="description" content="AstroV" />
 				<link rel="icon" href="/favicon.ico" />
+
+                <link rel="stylesheet" type="text/css" href="/fullPage/fullpage.css" />
+
                 <script src="/js/jquery.min.js"></script>
                 <script src="/js/main.js"></script>
+                <script src="/fullPage/fullpage.js"></script>
+
 			</Head>
 
-			<Section1/>
-                                <Section3 />
-                                <Section4 />
-                                <Section5 />
-                                <Section2 />
+			<div id="fullpage">
+                <Section1/>
+                <Section3/>
+                <Section4/>
+                <Section5/>
+                <Section2/>
+            </div>
 		</div>
 	)
 }

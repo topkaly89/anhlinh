c613747a277f34ddcf35b9e122588d276986d272
topkaly89 <allmylove00@gmail.com>
2021-09-14 22:08:16 +0700

    Add files via upload

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import '../styles/globals.css'
 import 'tailwindcss/tailwind.css'
-import 'js/jquery.min.js'
-import 'js/main.js'
+import '../js/jquery.min.js'
+import '../js/main.js'
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }

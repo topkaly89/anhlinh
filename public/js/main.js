0d894709ba5e0bb5e4d03ba1f4746036fbc7ed13
topkaly89 <allmylove00@gmail.com>
2021-09-15 03:40:13 +0700

    Add files via upload

--- public/js/main.js
+++ public/js/main.js
@@ -6,15 +6,10 @@ $(function(){
 
 
 
-    $('#fullpage').fullpage({
-		//options here
-		autoScrolling:true,
-		scrollHorizontally: true
-	});
-
-	//methods
-	$.fn.fullpage.setAllowScrolling(true);
-
+    new fullpage('#fullpage', {
+        anchors: ['page1', 'page2', 'page3', 'page4'],
+        sectionsColor: ['yellow', 'orange', '#C0C0C0', '#ADD8E6'],
+      });
 
 
 });

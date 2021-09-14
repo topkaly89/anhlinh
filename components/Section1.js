05b78d6fd3c1d18d63524a8a1212bf61d2fee714
topkaly89 <allmylove00@gmail.com>
2021-09-15 01:49:01 +0700

    Add files via upload

--- components/Section1.js
+++ components/Section1.js
@@ -63,6 +63,55 @@ export default function Section1() {
                         </div>
                         <div className="header-bottom-border"></div>
                     </div>
+
+
+                    <div className="flex justify-between">
+                            <div className="menu hide--desktop">
+                                <Image src={menu} width={60} id="menu"/>
+                            </div>
+                            <div className="socials flex items-center mb-4">
+                                <motion.a
+                                    initial={{opacity: 0}}
+                                    animate={{opacity: 1}}
+                                    transition={{ ease: "easeInOut", duration: 0.5, delay: 0.3 }}
+                                    className="social-item mr-4 flex items-center p-2 rounded-full transition-all bg-background hover:bg-backgroundAction cursor-pointer"
+                                    href="https://t.me/AstroGamingSocietyVN"
+                                    target="_blank"
+                                >
+                                    <Image src={telegram} width={18} height={18} />
+                                </motion.a>
+                                <motion.a
+                                    initial={{opacity: 0}}
+                                    animate={{opacity: 1}}
+                                    transition={{ ease: "easeInOut", duration: 0.5, delay: 0.5 }}
+                                    className="social-item mr-4 flex items-center p-2 rounded-full transition-all bg-background hover:bg-backgroundAction cursor-pointer"
+                                    href="https://discord.gg/vBJvumfvt5"
+                                    target="_blank"
+                                >
+                                    <Image src={discord} width={18} height={18} />
+                                </motion.a>
+                                <motion.a
+                                    initial={{opacity: 0}}
+                                    animate={{opacity: 1}}
+                                    transition={{ ease: "easeInOut", duration: 0.5, delay: 0.7 }}
+                                    className="social-item mr-4 flex items-center p-2 rounded-full transition-all bg-background hover:bg-backgroundAction cursor-pointer"
+                                    href="https://medium.com/@astrov.ventures"
+                                    target="_blank"
+                                >
+                                    <Image src={medium} width={18} height={18} />
+                                </motion.a>
+                                <motion.a
+                                    initial={{opacity: 0}}
+                                    animate={{opacity: 1}}
+                                    transition={{ ease: "easeInOut", duration: 0.5, delay: 0.9 }}
+                                    className="social-item flex items-center p-2 rounded-full transition-all bg-background hover:bg-backgroundAction cursor-pointer"
+                                >
+                                    <Image src={youtube} width={18} height={18} />
+                                </motion.a>
+                            </div>
+                        </div>
+
+
                     <div className="astrov-body mx-auto h-screen flex items-center justify-center flex-col px-4">
                         <motion.div
                             initial={{opacity: 0}}

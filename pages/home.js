9e86bead36b4230c5bb9773d46434ce9145e9f11
topkaly89 <allmylove00@gmail.com>
2021-09-15 03:28:32 +0700

    Add files via upload

--- pages/home.js
+++ pages/home.js
@@ -20,14 +20,13 @@ export default function Home() {
                 <script src="/fullPage/fullpage.js"></script>
 
 			</Head>
-            <body>
-                <div id="fullpage">
-                    <Section1/>
-                    <Section3/>
-                    <Section4/>
-                    <Section5/>
-                    <Section2/>
-                </div>
-            </body>
+
+			<div id="fullpage">
+                <Section1/>
+                <Section3/>
+                <Section4/>
+                <Section5/>
+                <Section2/>
+            </div>
 	)
 }

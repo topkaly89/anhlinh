d14d447d12d0fb7e8fd00a03d8a8dba15c7ebcd9
topkaly89 <allmylove00@gmail.com>
2021-09-15 12:50:39 +0700

    Add files via upload

--- components/Section1.js
+++ components/Section1.js
@@ -22,57 +22,50 @@ export default function Section1() {
                             <div class="dropdown">
                             <span><Image src={menu} width={24} height={24} id="menu1"/></span>
                             <div class="dropdown-content">
-                            <motion.div
-                            initial={{opacity: 0}}
-                            animate={{opacity: 1}}
-                            transition={{ ease: "easeOut", duration: 0.5, delay: 0.6 }}
-                            className="w-full hide--mobile" id="menulist">
-                            <div className="text-3xl md:text-5xl lg:text-6xl select-none d-none">TAKE OFF AND GO BEYOND</div>
-                            <div className="main-cta flex flex-col mt-10 lg:mt-12">
-                                <motion.div
-                                    initial={{opacity: 0}}
-                                    animate={{opacity: 1}}
-                                    transition={{ ease: "easeOut", duration: 0.5, delay: 0.8 }}
-                                    className="cta-item text-sm mb-3 lg:mb-4 lg:text-md md:text-left text-gray-400 hover:text-white transition-all cursor-pointer"
-                                >
-                                    ABOUT US
-                                </motion.div>
-                                <motion.div
-                                    initial={{opacity: 0}}
-                                    animate={{opacity: 1}}
-                                    transition={{ ease: "easeOut", duration: 0.5, delay: 0.9 }}
-                                    className="cta-item text-sm mb-3 lg:mb-4 lg:text-md md:text-left text-gray-400 hover:text-white transition-all cursor-pointer"
-                                >
-                                    > OUR SERVICES
-                                </motion.div>
-                                <motion.div
-                                    initial={{opacity: 0}}
-                                    animate={{opacity: 1}}
-                                    transition={{ ease: "easeOut", duration: 0.5, delay: 1 }}
-                                    className="cta-item text-sm mb-3 lg:mb-4 lg:text-md md:text-left text-gray-400 hover:text-white transition-all cursor-pointer"
-                                >
-                                    > PORTFOLIO
-                                </motion.div>
-                                <motion.a
-                                    initial={{opacity: 0}}
-                                    animate={{opacity: 1}}
-                                    transition={{ ease: "easeOut", duration: 0.5, delay: 1.2 }}
-                                    className="cta-item mb-3 lg:mb-4 text-sm lg:text-md md:text-left text-gray-400 hover:text-white transition-all cursor-pointer"
-                                    href="#contact"
-                                    //onClick={() => fullpageApi.moveSectionDown()}
-                                >
-                                    CONTACT US
-                                </motion.a>
-                                <motion.div
-                                    initial={{opacity: 0}}
-                                    animate={{opacity: 1}}
-                                    transition={{ ease: "easeOut", duration: 0.5, delay: 1.3 }}
-                                    className="cta-item text-sm lg:text-md md:text-left text-gray-400 hover:text-white transition-all cursor-pointer"
-                                >
-                                    ASTRONAUT CLUB (COMING SOON)
-                                </motion.div>
-                            </div>
-                        </motion.div>
+                                <div className="flex flex-col mt-10 lg:mt-12">
+                                    <motion.div
+                                        initial={{opacity: 0}}
+                                        animate={{opacity: 1}}
+                                        transition={{ ease: "easeOut", duration: 0.5, delay: 0.8 }}
+                                        className="cta-item text-sm mb-3 lg:mb-4 lg:text-md md:text-left text-gray-400 hover:text-white transition-all cursor-pointer"
+                                    >
+                                        ABOUT US
+                                    </motion.div>
+                                    <motion.div
+                                        initial={{opacity: 0}}
+                                        animate={{opacity: 1}}
+                                        transition={{ ease: "easeOut", duration: 0.5, delay: 0.9 }}
+                                        className="cta-item text-sm mb-3 lg:mb-4 lg:text-md md:text-left text-gray-400 hover:text-white transition-all cursor-pointer"
+                                    >
+                                        > OUR SERVICES
+                                    </motion.div>
+                                    <motion.div
+                                        initial={{opacity: 0}}
+                                        animate={{opacity: 1}}
+                                        transition={{ ease: "easeOut", duration: 0.5, delay: 1 }}
+                                        className="cta-item text-sm mb-3 lg:mb-4 lg:text-md md:text-left text-gray-400 hover:text-white transition-all cursor-pointer"
+                                    >
+                                        > PORTFOLIO
+                                    </motion.div>
+                                    <motion.a
+                                        initial={{opacity: 0}}
+                                        animate={{opacity: 1}}
+                                        transition={{ ease: "easeOut", duration: 0.5, delay: 1.2 }}
+                                        className="cta-item mb-3 lg:mb-4 text-sm lg:text-md md:text-left text-gray-400 hover:text-white transition-all cursor-pointer"
+                                        href="#contact"
+                                        //onClick={() => fullpageApi.moveSectionDown()}
+                                    >
+                                        CONTACT US
+                                    </motion.a>
+                                    <motion.div
+                                        initial={{opacity: 0}}
+                                        animate={{opacity: 1}}
+                                        transition={{ ease: "easeOut", duration: 0.5, delay: 1.3 }}
+                                        className="cta-item text-sm lg:text-md md:text-left text-gray-400 hover:text-white transition-all cursor-pointer"
+                                    >
+                                        ASTRONAUT CLUB (COMING SOON)
+                                    </motion.div>
+                                </div>
                             </div>
                             </div>
                                 

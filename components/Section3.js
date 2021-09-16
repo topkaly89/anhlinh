7973b92e0856a73471996ebcfb6e86b9fecc75e4
topkaly89 <allmylove00@gmail.com>
2021-09-17 03:05:38 +0700

    Add files via upload

--- components/Section3.js
+++ components/Section3.js
@@ -9,8 +9,8 @@ export default function Section3() {
     return (
         <div className="section w-full bg_rotate">
             <div className="container px-4">
-            <div className="info row gx-5 gy-5 items-center">
-                    <div className="col-12 col-sm-12 col-md-6 col-lg-6">
+            <div className="info row gx-5 gy-5 items-center m-auto">
+                    <div className="col-12 col-sm-12 col-md-6 col-lg-6 z-30">
                         <h2>Why AstroV?</h2>
                         <p>
                         AstroV is deeply committed to providing effective support and long term value to blockchain games and technology.
@@ -19,7 +19,7 @@ export default function Section3() {
                         Our ethos strongly aligns with teams that have the desire to build industry changing platforms, and we pride ourselves in having a large range of resources to realise our shared visions.
                         </p>
                     </div>
-                    <div className="col-12 col-sm-12 col-md-6 col-lg-6 text-end">
+                    <div className="col-12 col-sm-12 col-md-6 col-lg-6 text-end z-30">
                         <Image src={s2} width={525} />
                     </div>
                 </div>
--- components/Section4.js
+++ components/Section4.js
@@ -9,11 +9,11 @@ export default function Section4() {
     return (
         <div className="section w-full bg">
             <div className="container px-4">
-                    <div className="info row gx-5 gy-5 items-center">
-                        <div className="col-12 col-sm-12 col-md-6 col-lg-6 text-start">
+                    <div className="info row gx-5 gy-5 items-center m-auto">
+                        <div className="col-12 col-sm-12 col-md-6 col-lg-6 text-start z-30">
                             <Image src={s3} width={525} />
                         </div>
-                        <div className="col-12 col-sm-12 col-md-6 col-lg-6">
+                        <div className="col-12 col-sm-12 col-md-6 col-lg-6 z-30">
                             <h2>What we do</h2>
                             <p>
                             AstroV Ventures invests in potential gaming, NFT and DeFi projects. Beside financial investments, we are dedicated in helping blockchain projects reach mass adoption by providing extensive resources and industry networks.

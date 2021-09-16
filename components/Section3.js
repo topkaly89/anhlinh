65d12273b39493a8322edf92fff7c822d7d69610
topkaly89 <allmylove00@gmail.com>
2021-09-17 02:38:14 +0700

    Add files via upload

--- components/Section3.js
+++ components/Section3.js
@@ -10,20 +10,18 @@ export default function Section3() {
         <div className="section w-full bg">
             <div class="bg_rotate"></div>
             <div className="container px-4">
-                <div className="flex items-center justify-between w-full">
-                    <div className="info items-center m-auto">
-                        <div className="col-12 col-sm-12 col-md-6 col-lg-6">
-                            <h2>Why AstroV?</h2>
-                            <p>
-                            AstroV is deeply committed to providing effective support and long term value to blockchain games and technology.
-                            </p>
-                            <p>
-                            Our ethos strongly aligns with teams that have the desire to build industry changing platforms, and we pride ourselves in having a large range of resources to realise our shared visions.
-                            </p>
-                        </div>
-                        <div className="col-12 col-sm-12 col-md-6 col-lg-6 text-end">
-                            <Image src={s2} width={525} />
-                        </div>
+                <div className="info items-center m-auto">
+                    <div className="col-12 col-sm-12 col-md-6 col-lg-6">
+                        <h2>Why AstroV?</h2>
+                        <p>
+                        AstroV is deeply committed to providing effective support and long term value to blockchain games and technology.
+                        </p>
+                        <p>
+                        Our ethos strongly aligns with teams that have the desire to build industry changing platforms, and we pride ourselves in having a large range of resources to realise our shared visions.
+                        </p>
+                    </div>
+                    <div className="col-12 col-sm-12 col-md-6 col-lg-6 text-end">
+                        <Image src={s2} width={525} />
                     </div>
                 </div>
             </div>

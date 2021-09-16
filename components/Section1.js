393954de2a4f412e06cd23be645849e48f4018e8
topkaly89 <allmylove00@gmail.com>
2021-09-16 22:39:38 +0700

    Add files via upload

--- components/Section1.js
+++ components/Section1.js
@@ -1,12 +1,6 @@
 import Image from 'next/image'
 import { motion } from "framer-motion"
 
-// Social logos
-import telegram from '../public/socials/telegram.svg'
-import twitter from '../public/socials/twitter.svg'
-import discord from '../public/socials/discord.svg'
-import medium from '../public/socials/medium.svg'
-import menu from '../public/menu.svg'
 
 export default function Section1() {
     //var fullpageApi = props.fullpageApi
@@ -16,108 +10,7 @@ export default function Section1() {
             <div className="w-full flex flex-col">
                 <div className="container px-4">
                     <div className="astrov-body mx-auto h-screen flex items-center justify-center flex-col">
-                    <div className="header py-4 w-full">
-                        <div className="flex items-center justify-between mb-4">
-                            <div className="menu flex items-center hide--desktop">
-                            <div class="dropdown">
-                            <span><Image src={menu} width={24} height={24} id="menu1"/></span>
-                            <div class="dropdown-content">
-                                <div className="flex flex-col">
-                                    <motion.div
-                                        initial={{opacity: 0}}
-                                        animate={{opacity: 1}}
-                                        transition={{ ease: "easeOut", duration: 0.5, delay: 0.8 }}
-                                        className="moveTo cta-item text-sm mb-3 lg:mb-4 lg:text-md md:text-left text-gray-400 hover:text-white transition-all cursor-pointer"
-                                        data-section="2"
-                                    >
-                                        ABOUT US
-                                    </motion.div>
-                                    <motion.div
-                                        initial={{opacity: 0}}
-                                        animate={{opacity: 1}}
-                                        transition={{ ease: "easeOut", duration: 0.5, delay: 0.9 }}
-                                        className="moveTo cta-item text-sm mb-3 lg:mb-4 lg:text-md md:text-left text-gray-400 hover:text-white transition-all cursor-pointer"
-                                        data-section="3"
-                                    >
-                                        > OUR SERVICES
-                                    </motion.div>
-                                    <motion.div
-                                        initial={{opacity: 0}}
-                                        animate={{opacity: 1}}
-                                        transition={{ ease: "easeOut", duration: 0.5, delay: 1 }}
-                                        className="moveTo cta-item text-sm mb-3 lg:mb-4 lg:text-md md:text-left text-gray-400 hover:text-white transition-all cursor-pointer"
-                                        data-section="4"
-                                    >
-                                        > PORTFOLIO
-                                    </motion.div>
-                                    <motion.div
-                                        initial={{opacity: 0}}
-                                        animate={{opacity: 1}}
-                                        transition={{ ease: "easeOut", duration: 0.5, delay: 1.2 }}
-                                        className="moveTo cta-item mb-3 lg:mb-4 text-sm lg:text-md md:text-left text-gray-400 hover:text-white transition-all cursor-pointer"
-                                        data-section="5"
-                                        //onClick={() => fullpageApi.moveSectionDown()}
-                                    >
-                                        CONTACT US
-                                    </motion.div>
-                                    <motion.div
-                                        initial={{opacity: 0}}
-                                        animate={{opacity: 1}}
-                                        transition={{ ease: "easeOut", duration: 0.5, delay: 1.3 }}
-                                        className="cta-item text-sm lg:text-md md:text-left text-gray-400 hover:text-white transition-all cursor-pointer"
-                                    >
-                                        ASTRONAUT CLUB (COMING SOON)
-                                    </motion.div>
-                                </div>
-                            </div>
-                            </div>
-                                
-                            </div>
-                            <div className="socials flex items-center">
-                                <motion.a
-                                    initial={{opacity: 0}}
-                                    animate={{opacity: 1}}
-                                    transition={{ ease: "easeInOut", duration: 0.5, delay: 0.3 }}
-                                    className="social-item mr-4 flex items-center p-2 rounded-full transition-all bg-background hover:bg-backgroundAction cursor-pointer"
-                                    href="https://t.me/AstroGamingSocietyVN"
-                                    target="_blank"
-                                >
-                                    <Image src={telegram} width={18} height={18} />
-                                </motion.a>
-                                <motion.a
-                                    initial={{opacity: 0}}
-                                    animate={{opacity: 1}}
-                                    transition={{ ease: "easeInOut", duration: 0.5, delay: 0.5 }}
-                                    className="social-item mr-4 flex items-center p-2 rounded-full transition-all bg-background hover:bg-backgroundAction cursor-pointer"
-                                    href="https://discord.gg/vBJvumfvt5"
-                                    target="_blank"
-                                >
-                                    <Image src={discord} width={18} height={18} />
-                                </motion.a>
-                                <motion.a
-                                    initial={{opacity: 0}}
-                                    animate={{opacity: 1}}
-                                    transition={{ ease: "easeInOut", duration: 0.5, delay: 0.7 }}
-                                    className="social-item mr-4 flex items-center p-2 rounded-full transition-all bg-background hover:bg-backgroundAction cursor-pointer"
-                                    href="https://medium.com/@astrov.ventures"
-                                    target="_blank"
-                                >
-                                    <Image src={medium} width={18} height={18} />
-                                </motion.a>
-                                <motion.a
-                                    initial={{opacity: 0}}
-                                    animate={{opacity: 1}}
-                                    transition={{ ease: "easeInOut", duration: 0.5, delay: 0.9 }}
-                                    className="social-item flex items-center p-2 rounded-full transition-all bg-background hover:bg-backgroundAction cursor-pointer"
-                                    href="https://twitter.com/AstrovVentures"
-                                    target="_blank"
-                                >
-                                    <Image src={twitter} width={18} height={18} />
-                                </motion.a>
-                            </div>
-                        </div>
-                        <div className="header-bottom-border"></div>
-                    </div>
+                    
                         <motion.div
                             initial={{opacity: 0}}
                             animate={{opacity: 1}}
--- components/Section6.js
+++ components/Section6.js
@@ -6,57 +6,112 @@ import telegram from '../public/socials/telegram.svg'
 import twitter from '../public/socials/twitter.svg'
 import discord from '../public/socials/discord.svg'
 import medium from '../public/socials/medium.svg'
-
+import menu from '../public/menu.svg'
 
 export default function Section6() {
     return (
-        <div className="section fp-auto-height w-full bg is_footer">
-            <div className="section-footer">
-                <div className="header-bottom-border"></div>
-                
-                <div className="footer-body flex items-center">
-                    <div className="footer-text">Copyright © 2021 AstroV. All rights reserved.</div>
-                    <div className="socials flex items-center mb-4">
-                        <motion.a
+        <div className="header w-full">
+            <div className="container px-4">
+            <div className="flex items-center justify-between mb-4">
+                <div className="menu flex items-center hide--desktop">
+                <div class="dropdown">
+                <span><Image src={menu} width={24} height={24} id="menu1"/></span>
+                <div class="dropdown-content">
+                    <div className="flex flex-col">
+                        <motion.div
                             initial={{opacity: 0}}
                             animate={{opacity: 1}}
-                            transition={{ ease: "easeInOut", duration: 0.5, delay: 0.3 }}
-                            className="social-item mr-4 flex items-center p-2 rounded-full transition-all bg-background hover:bg-backgroundAction cursor-pointer"
-                            href="https://t.me/AstroGamingSocietyVN"
-                            target="_blank"
+                            transition={{ ease: "easeOut", duration: 0.5, delay: 0.8 }}
+                            className="moveTo cta-item text-sm mb-3 lg:mb-4 lg:text-md md:text-left text-gray-400 hover:text-white transition-all cursor-pointer"
+                            data-section="2"
                         >
-                            <Image src={telegram} width={18} height={18} />
-                        </motion.a>
-                        <motion.a
+                            ABOUT US
+                        </motion.div>
+                        <motion.div
                             initial={{opacity: 0}}
                             animate={{opacity: 1}}
-                            transition={{ ease: "easeInOut", duration: 0.5, delay: 0.5 }}
-                            className="social-item mr-4 flex items-center p-2 rounded-full transition-all bg-background hover:bg-backgroundAction cursor-pointer"
-                            href="https://discord.gg/vBJvumfvt5"
-                            target="_blank"
+                            transition={{ ease: "easeOut", duration: 0.5, delay: 0.9 }}
+                            className="moveTo cta-item text-sm mb-3 lg:mb-4 lg:text-md md:text-left text-gray-400 hover:text-white transition-all cursor-pointer"
+                            data-section="3"
                         >
-                            <Image src={discord} width={18} height={18} />
-                        </motion.a>
-                        <motion.a
+                            > OUR SERVICES
+                        </motion.div>
+                        <motion.div
                             initial={{opacity: 0}}
                             animate={{opacity: 1}}
-                            transition={{ ease: "easeInOut", duration: 0.5, delay: 0.7 }}
-                            className="social-item mr-4 flex items-center p-2 rounded-full transition-all bg-background hover:bg-backgroundAction cursor-pointer"
-                            href="https://medium.com/@astrov.ventures"
-                            target="_blank"
+                            transition={{ ease: "easeOut", duration: 0.5, delay: 1 }}
+                            className="moveTo cta-item text-sm mb-3 lg:mb-4 lg:text-md md:text-left text-gray-400 hover:text-white transition-all cursor-pointer"
+                            data-section="4"
                         >
-                            <Image src={medium} width={18} height={18} />
-                        </motion.a>
-                        <motion.a
+                            > PORTFOLIO
+                        </motion.div>
+                        <motion.div
                             initial={{opacity: 0}}
                             animate={{opacity: 1}}
-                            transition={{ ease: "easeInOut", duration: 0.5, delay: 0.9 }}
-                            className="social-item flex items-center p-2 rounded-full transition-all bg-background hover:bg-backgroundAction cursor-pointer"
+                            transition={{ ease: "easeOut", duration: 0.5, delay: 1.2 }}
+                            className="moveTo cta-item mb-3 lg:mb-4 text-sm lg:text-md md:text-left text-gray-400 hover:text-white transition-all cursor-pointer"
+                            data-section="5"
+                            //onClick={() => fullpageApi.moveSectionDown()}
                         >
-                            <Image src={twitter} width={18} height={18} />
-                        </motion.a>
+                            CONTACT US
+                        </motion.div>
+                        <motion.div
+                            initial={{opacity: 0}}
+                            animate={{opacity: 1}}
+                            transition={{ ease: "easeOut", duration: 0.5, delay: 1.3 }}
+                            className="cta-item text-sm lg:text-md md:text-left text-gray-400 hover:text-white transition-all cursor-pointer"
+                        >
+                            ASTRONAUT CLUB (COMING SOON)
+                        </motion.div>
                     </div>
                 </div>
+                </div>
+                    
+                </div>
+                <div className="socials flex items-center">
+                    <motion.a
+                        initial={{opacity: 0}}
+                        animate={{opacity: 1}}
+                        transition={{ ease: "easeInOut", duration: 0.5, delay: 0.3 }}
+                        className="social-item mr-4 flex items-center p-2 rounded-full transition-all bg-background hover:bg-backgroundAction cursor-pointer"
+                        href="https://t.me/AstroGamingSocietyVN"
+                        target="_blank"
+                    >
+                        <Image src={telegram} width={18} height={18} />
+                    </motion.a>
+                    <motion.a
+                        initial={{opacity: 0}}
+                        animate={{opacity: 1}}
+                        transition={{ ease: "easeInOut", duration: 0.5, delay: 0.5 }}
+                        className="social-item mr-4 flex items-center p-2 rounded-full transition-all bg-background hover:bg-backgroundAction cursor-pointer"
+                        href="https://discord.gg/vBJvumfvt5"
+                        target="_blank"
+                    >
+                        <Image src={discord} width={18} height={18} />
+                    </motion.a>
+                    <motion.a
+                        initial={{opacity: 0}}
+                        animate={{opacity: 1}}
+                        transition={{ ease: "easeInOut", duration: 0.5, delay: 0.7 }}
+                        className="social-item mr-4 flex items-center p-2 rounded-full transition-all bg-background hover:bg-backgroundAction cursor-pointer"
+                        href="https://medium.com/@astrov.ventures"
+                        target="_blank"
+                    >
+                        <Image src={medium} width={18} height={18} />
+                    </motion.a>
+                    <motion.a
+                        initial={{opacity: 0}}
+                        animate={{opacity: 1}}
+                        transition={{ ease: "easeInOut", duration: 0.5, delay: 0.9 }}
+                        className="social-item flex items-center p-2 rounded-full transition-all bg-background hover:bg-backgroundAction cursor-pointer"
+                        href="https://twitter.com/AstrovVentures"
+                        target="_blank"
+                    >
+                        <Image src={twitter} width={18} height={18} />
+                    </motion.a>
+                </div>
+            </div>
+            <div className="header-bottom-border"></div>
             </div>
         </div>
     )

ad21b5758ff3e5ac3254e366d0483d8c9f34bd59
topkaly89 <allmylove00@gmail.com>
2021-09-17 02:33:00 +0700

    Add files via upload

--- components/Section3.js
+++ components/Section3.js
@@ -11,7 +11,7 @@ export default function Section3() {
             <div class="bg_rotate"></div>
             <div className="container px-4">
                 <div className="flex items-center justify-between w-full">
-                    <div className="info row gx-5 gy-5 m-auto">
+                    <div className="info items-center m-auto">
                         <div className="col-12 col-sm-12 col-md-6 col-lg-6">
                             <h2>Why AstroV?</h2>
                             <p>

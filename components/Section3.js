20c9522833d95dabd2022e465035fa70af94511e
topkaly89 <allmylove00@gmail.com>
2021-09-16 23:34:21 +0700

    Add files via upload

--- components/Section3.js
+++ components/Section3.js
@@ -21,7 +21,15 @@ export default function Section3() {
                             </p>
                         </div>
                         <div className="col-12 col-sm-12 col-md-6 col-lg-6 text-end">
-                            <Image src={s2} width={525} />
+                            <motion.img
+                                initial={false}
+                                animate={{ y: isHovered ? -30 : 0 }}
+                                transition={{ ease: "easeOut", duration: 0.5 }}
+                                src={s2}
+                                width={525}
+                                onMouseEnter={() => setHovered(true)}
+                                onMouseLeave={() => setHovered(false)}
+                            />
                         </div>
                     </div>
                 </div>

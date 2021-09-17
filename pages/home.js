0e13c5110e690b545867b1f7956ed87f2c75fe4f
topkaly89 <allmylove00@gmail.com>
2021-09-17 10:03:11 +0700

    Add files via upload

--- pages/home.js
+++ pages/home.js
@@ -6,6 +6,7 @@ import Section2 from '../components/Section2'
 import Section3 from '../components/Section3'
 import Section4 from '../components/Section4'
 import Section5 from '../components/Section5'
+import Section6 from '../components/Section6'
 export default function Home() {
 	return (
 		<div>
@@ -23,7 +24,7 @@ export default function Home() {
                 
 
 			</Head>
-
+            <Section6/>
 			<div id="fullpage">
                 <Section1/>
                 <Section3/>

6f8c96e7975591c6c98a6b51ece92c1ce946eb86
topkaly89 <allmylove00@gmail.com>
2021-09-17 08:21:40 +0700

    Add files via upload

--- components/Section3.js
+++ components/Section3.js
@@ -8,21 +8,23 @@ export default function Section3() {
 
     return (
         <div className="section w-full bg_rotate">
-            <div className="container px-4">
-            <div className="info info1 row gx-5 gy-5 items-center m-auto">
-                    <div className="col-12 col-sm-12 col-md-6 col-lg-6 z-30">
-                        <h2>Why AstroV?</h2>
-                        <p>
-                        AstroV is deeply committed to providing effective support and long term value to blockchain games and technology.
-                        </p>
-                        <p>
-                        Our ethos strongly aligns with teams that have the desire to build industry changing platforms, and we pride ourselves in having a large range of resources to realise our shared visions.
-                        </p>
-                    </div>
-                    <div className="col-12 col-sm-12 col-md-6 col-lg-6 text-end z-30">
-                        <motion.img
-                            src="/s2.svg" width="525" height="525" data-speed="-5"
-                        />
+            <div className="info1">
+                <div className="container px-4">
+                    <div className="info row gx-5 gy-5 items-center m-auto">
+                        <div className="col-12 col-sm-12 col-md-6 col-lg-6 z-30">
+                            <h2>Why AstroV?</h2>
+                            <p>
+                            AstroV is deeply committed to providing effective support and long term value to blockchain games and technology.
+                            </p>
+                            <p>
+                            Our ethos strongly aligns with teams that have the desire to build industry changing platforms, and we pride ourselves in having a large range of resources to realise our shared visions.
+                            </p>
+                        </div>
+                        <div className="col-12 col-sm-12 col-md-6 col-lg-6 text-end z-30">
+                            <motion.img
+                                src="/s2.svg" width="525" height="525" data-speed="-5"
+                            />
+                        </div>
                     </div>
                 </div>
             </div>

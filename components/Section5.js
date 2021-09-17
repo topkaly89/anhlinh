482cb46be07b59ccd1bcdb5c77ba141fa55e8f69
loc le <tienloc.le2604@gmail.com>
2021-09-17 18:12:32 +0700

    title

--- components/Section5.js
+++ components/Section5.js
@@ -5,6 +5,8 @@ import Image from 'next/image'
 import s4 from '../public/s4.svg'
 import s5 from '../public/s5.svg'
 import s6 from '../public/s6.svg'
+import coin98 from '../public/coin98svg.svg'
+import defi from '../public/defiwarrior.svg'
 
 export default function Section5() {
 
@@ -17,8 +19,8 @@ export default function Section5() {
                             <div className="h-title">Portfolio</div>
                         </div>
                         <div className="col-6 col-sm-6 col-md-4 col-lg-2">
-                            <div className="portfolio">
-                                <div className="img hover:box-shadow-light">
+                            <div className="portfolio hover:box-shadow-light">
+                                <div className="img ">
                                     <Image src={s4}/>
                                 </div>
                                 <div className="title">DotOracle</div>
@@ -26,18 +28,18 @@ export default function Section5() {
                         </div>
 
                         <div className="col-6 col-sm-6 col-md-4 col-lg-2">
-                            <div className="portfolio">
-                                <div className="img hover:box-shadow-light">
-                                    <Image src={s6}/>
+                            <div className="portfolio hover:box-shadow-light">
+                                <div className="img " id="coin98">
+                                    <Image src={coin98}/>
                                 </div>
                                 <div className="title">Coin98</div>
                             </div>
                         </div>
 
                         <div className="col-6 col-sm-6 col-md-4 col-lg-2">
-                            <div className="portfolio">
-                                <div className="img hover:box-shadow-light">
-                                    <Image src={s5}/>
+                            <div className="portfolio hover:box-shadow-light">
+                                <div className="img " id="defiwarrior">
+                                    <Image src={defi}/>
                                 </div>
                                 <div className="title">Defi Warrior</div>
                             </div>
